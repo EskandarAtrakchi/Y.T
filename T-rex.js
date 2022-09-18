f6ba9f9d67351046cb8a538d2e57e90d2d7c5c52
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-09-18 20:32:50 +0100

    T-rex

--- T-rex.js
+++ T-rex.js
@@ -9,37 +9,37 @@ Runner.instance_.tRex.setJumpVelocity(100)
 Runner.instance_.setSpeed(1000)
 
 */
-function magic(){
+function followMeForMore(){
  var rCanvas = document.getElementsByClassName("runner-canvas")[0];
  var runnerCanvasContext = rCanvas.getContext('2d');
- var imgd = runnerCanvasContext.getImageData(rCanvas.width/8, 
-rCanvas.height - rCanvas.height/2.5, rCanvas.width/7, rCanvas.height/5);
+ var objXYZ = runnerCanvasContext.getImageData(rCanvas.width/8.002, 
+rCanvas.height - rCanvas.height/2.51, rCanvas.width/7, rCanvas.height/5.01);
  var jumpFlag = false;
- var pix = imgd.data;
- for (var i = 0, n = pix.length; i < n; i += 4) {
-  if(pix[i]>0||pix[i+1]>0||pix[i+2]>0){
+ var MovingInstance = objXYZ.data;
+ for (var i = 0, n = MovingInstance.length; i < n; i += 4) {
+  if(MovingInstance[i]>0||MovingInstance[i+1]>0||MovingInstance[i+2]>0){
    jumpFlag = true;
    break;
   }
  }
  if(jumpFlag){
   simulateJump();
-  return setTimeout(function(){magic();},100);
+  return setTimeout(function(){followMeForMore();},100);
  } else {
-  setTimeout(function(){magic();},50);
+  setTimeout(function(){followMeForMore();},51);
  }
 };
 
 function simulateJump() {
- var keyEvent = document.createEvent('KeyboardEvent');
- Object.defineProperty(keyEvent, 'keyCode', {
+ var Initi = document.createEvent('KeyboardEvent');
+ Object.defineProperty(Initi, 'keyCode', {
   get : function() {
    return this.keyCodeVal;
   }
  });
- keyEvent.initKeyboardEvent
+ Initi.initKeyboardEvent
 ('keydown', true, false, null, 0, false, 0, false, 38, 0);
- keyEvent.keyCodeVal = 38;
- document.dispatchEvent(keyEvent);
+ Initi.keyCodeVal = 38;
+ document.dispatchEvent(Initi);
 }
-magic();
+followMeForMore();
