a840ba1cec45b3281fb21a4540a225c06915b47a
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-09-30 18:44:23 +0100

    AI

--- AI.js
+++ AI.js
@@ -1,35 +1,35 @@
-
 function followMeForMore(){
- var xCanvas = document.getElementsByClassName("runner-canvas")[0];
- var runnexCanvasContext = xCanvas.getContext('2d');
- var objXY = runnexCanvasContext.getImageData(xCanvas.width/8.002, 
-    xCanvas.height - xCanvas.height/2.51, xCanvas.width/7, xCanvas.height/5.01);
- var jumpFlag = false;
- var MovingInstance = objXY.data;
- for (var i = 0, n = MovingInstance.length; i < n; i += 4) {
-  if(MovingInstance[i]>0||MovingInstance[i+1]>0||MovingInstance[i+2]>0){
-   jumpFlag = true;
-   break;
+   var xCanvas = document.getElementsByClassName("runner-canvas")[0];
+   var runnexCanvasContext = xCanvas.getContext('2d');
+   var objXY = runnexCanvasContext.getImageData(xCanvas.width/8.002, 
+      xCanvas.height - xCanvas.height/2.51, xCanvas.width/7, xCanvas.height/5.01);
+   var jumpFlag = false;
+   var autoJump = objXY.data;
+   for (var i = 0, n = autoJump.length; i < n; i += 4) {
+    if(autoJump[i]>0||autoJump[i+1]>0||autoJump[i+2]>0){
+     jumpFlag = true;
+     break;
+    }
+   }
+   if(jumpFlag){
+    simulateJump();
+    return setTimeout(function(){followMeForMore();
+   },100);
+   } else {
+    setTimeout(function(){followMeForMore();},51);
+   }
+  };
+  
+  function simulateJump() {
+   var Initi = document.createEvent('KeyboardEvent');
+   Object.defineProperty(Initi, 'keyCode', {
+    get : function() {
+     return this.keyCodeVal;
+    }
+   });
+   Initi.initKeyboardEvent
+  ('keydown', true, false, null, 0, false, 0, false, 38, 0);
+   Initi.keyCodeVal = 38;
+   document.dispatchEvent(Initi);
   }
- }
- if(jumpFlag){
-  simulateJump();
-  return setTimeout(function(){followMeForMore();},100);
- } else {
-  setTimeout(function(){followMeForMore();},51);
- }
-};
-
-function simulateJump() {
- var Initi = document.createEvent('KeyboardEvent');
- Object.defineProperty(Initi, 'keyCode', {
-  get : function() {
-   return this.keyCodeVal;
-  }
- });
- Initi.initKeyboardEvent
-('keydown', true, false, null, 0, false, 0, false, 38, 0);
- Initi.keyCodeVal = 38;
- document.dispatchEvent(Initi);
-}
-followMeForMore();
+  followMeForMore();
