3185d66abe9718b2d9d5f0377844b9a3f53228e6
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-10-03 00:51:17 +0100

    ASCII

---
+++ ASCII.java
@@ -0,0 +1,80 @@
+//American Standard Code for Information Interchange (ASCII)
+//this code was made by Eskandar Atrakchi
+import java.util.Scanner;
+ 
+class Micc {
+	static String struserInput;
+
+	public static void main(String[] args) {
+	
+		actual();
+		Main();
+		exit();
+		info();
+	}
+	public static void actual() {
+		Scanner y = new Scanner (System.in);
+		System.out.println("Select number 1 to choose an Alphabet\nor number 2 to exit");
+
+		while(true) {
+			try {
+				int x = y.nextInt();
+				System.out.println("Please confirm your number again ");
+			}
+			catch (Exception e) {
+				System.out.println("Something went wrong! please, choose again ");actual();
+			}
+			int x = y.nextInt();
+			switch(x) {
+			case 1 : 
+				Main();System.out.println("You can select another Alphbet or select number 2 to exit the system ");break;
+			case 2 : System.out.println("The system is shutted down"); exit();break;
+			default : System.out.println("Please try again "); actual();
+			}
+		}
+	}
+	
+	public static void Main() {
+		System.out.println("Please enter the Alphabet");
+		Scanner scanner = new Scanner (System.in);
+		char ch = scanner.next().charAt(0);
+		int ASCII = ch;
+		
+		System.out.println("The value of " + ch + " is " + ASCII);
+	}
+	public static void exit() {
+		System.exit(0);
+	}
+	public static void info() {
+		System.out.println("Wrong input!");
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
