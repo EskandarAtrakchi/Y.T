486935c575a80b1c049553c7785acb48e55f9ef2
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-02-14 02:22:30 +0000

    .

--- Bollinger_band_strategy.r
+++ Bollinger_band_strategy.r
@@ -4,38 +4,51 @@
 // that the 1st and 2nd StdDev are outlined with two colors and two
 // different levels, one for each Standard Deviation
 
+// Define the strategy settings
 strategy(shorttitle="MBB", title="Bollinger Bands", overlay=true)
-src = input(close)
-length = input.int(34, minval=1)
-mult = input.float(2.0, minval=0.001, maxval=50)
 
+// Define the input options
+src = input(close, "Source")
+length = input.int(34, "Length", minval=1)
+mult = input.float(2.0, "Multiplier", minval=0.001, maxval=50)
+
+// Calculate the basis, deviation, and upper and lower bands
 basis = ta.sma(src, length)
 dev = ta.stdev(src, length)
-dev2 = mult*dev
+dev2 = mult * dev
 
 upper1 = basis + dev
 lower1 = basis - dev
 upper2 = basis + dev2
 lower2 = basis - dev2
 
+// Define the plot colors and styles
 colorBasis = src >= basis ? color.blue : color.orange
+colorUpper = color.new(color.blue, 80)
+colorLower = color.new(color.orange, 80)
+styleCircles = plot.style_circles
 
-pBasis = plot(basis, linewidth=2, color=colorBasis)
-pUpper1 = plot(upper1, color =  color.new(color.blue, 0), style =  plot.style_circles)
-pUpper2 = plot(upper2, color= color.new(color.blue, 0))
-pLower1 = plot(lower1, color = color.new(color.orange, 0), style =  plot.style_circles)
-pLower2 = plot(lower2, color = color.new(color.orange, 0))
+// Plot the Bollinger Bands and fill between the bands
+pBasis = plot(basis, "Basis", linewidth=2, color=colorBasis)
+pUpper1 = plot(upper1, "Upper1", color=colorUpper, style=styleCircles)
+pUpper2 = plot(upper2, "Upper2", color=colorUpper)
+pLower1 = plot(lower1, "Lower1", color=colorLower, style=styleCircles)
+pLower2 = plot(lower2, "Lower2", color=colorLower)
 
-fill(pBasis,pUpper2, color = color.new(color.blue, 80))
-fill(pUpper1, pUpper2, color = color.new(color.blue, 80))
-fill(pBasis,pLower2, color = color.new(color.orange, 80))
-fill(pLower1, pLower2, color = color.new(color.orange, 80))
+fill(pBasis, pUpper2, color=colorUpper)
+fill(pUpper1, pUpper2, color=colorUpper)
+fill(pBasis, pLower2, color=colorLower)
+fill(pLower1, pLower2, color=colorLower)
 
+// Define the strategy rules
 if(close > upper2)
     strategy.entry("Long", strategy.long)
+
 if(close < lower2)
     strategy.entry("Short", strategy.short)
+
 if(close <= lower2)
     strategy.close("Long")
+
 if(close >= upper2)
     strategy.close("Short")
