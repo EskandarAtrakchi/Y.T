5da86f1df00e356d93742eef465c577f660f5368
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2024-03-20 11:35:15 +0000

    Update getSpamUnreadCount.js

--- getSpamUnreadCount.js
+++ getSpamUnreadCount.js
@@ -1,6 +1,7 @@
 function myFunction() {
-    // Unless you actually read stuff in your spam folder, this should be the same as
-  // the number of messages in your spam folder.
-  Logger.log("# unread threads that are spam: " + GmailApp.getSpamUnreadCount());
+    /* Unless you actually read stuff in your spam folder, this should be the same as
+    * the number of messages in spam folder.
+    */
+    Logger.log("# unread threads that are spam: " + GmailApp.getSpamUnreadCount());
   }
   
