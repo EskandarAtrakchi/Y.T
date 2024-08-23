934773d1930b4cd2afd72809ba5af8a4f5eb5fe6
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2024-08-23 02:01:07 +0100

    Update RSI.r

--- TradingView/RSI.r
+++ TradingView/RSI.r
@@ -1,50 +1,73 @@
 //@version=5
-indicator(title="RSI Divergence Indicator", format=format.price, timeframe="", timeframe_gaps=true)
-len = input.int(title="RSI Period", minval=1, defval=14)
-src = input(title="RSI Source", defval=close)
-lbR = input(title="Pivot Lookback Right", defval=5, display = display.data_window)
-lbL = input(title="Pivot Lookback Left", defval=5, display = display.data_window)
-rangeUpper = input(title="Max of Lookback Range", defval=60, display = display.data_window)
-rangeLower = input(title="Min of Lookback Range", defval=5, display = display.data_window)
-plotBull = input(title="Plot Bullish", defval=true, display = display.data_window)
-plotHiddenBull = input(title="Plot Hidden Bullish", defval=false, display = display.data_window)
-plotBear = input(title="Plot Bearish", defval=true, display = display.data_window)
-plotHiddenBear = input(title="Plot Hidden Bearish", defval=false, display = display.data_window)
+indicator(title="Relative Strength Index", shorttitle="RSI", format=format.price, precision=2, timeframe="", timeframe_gaps=true)
+
+ma(source, length, type) =>
+    switch type
+        "SMA" => ta.sma(source, length)
+        "Bollinger Bands" => ta.sma(source, length)
+        "EMA" => ta.ema(source, length)
+        "SMMA (RMA)" => ta.rma(source, length)
+        "WMA" => ta.wma(source, length)
+        "VWMA" => ta.vwma(source, length)
+
+rsiLengthInput = input.int(14, minval=1, title="RSI Length", group="RSI Settings")
+rsiSourceInput = input.source(close, "Source", group="RSI Settings")
+maTypeInput = input.string("SMA", title="MA Type", options=["SMA", "Bollinger Bands", "EMA", "SMMA (RMA)", "WMA", "VWMA"], group="MA Settings", display = display.data_window)
+maLengthInput = input.int(14, title="MA Length", group="MA Settings", display = display.data_window)
+bbMultInput = input.float(2.0, minval=0.001, maxval=50, title="BB StdDev", group="MA Settings", display = display.data_window)
+showDivergence = input.bool(false, title="Show Divergence", group="RSI Settings", display = display.data_window)
+
+up = ta.rma(math.max(ta.change(rsiSourceInput), 0), rsiLengthInput)
+down = ta.rma(-math.min(ta.change(rsiSourceInput), 0), rsiLengthInput)
+rsi = down == 0 ? 100 : up == 0 ? 0 : 100 - (100 / (1 + up / down))
+rsiMA = ma(rsi, maLengthInput, maTypeInput)
+isBB = maTypeInput == "Bollinger Bands"
+
+rsiPlot = plot(rsi, "RSI", color=#7E57C2)
+plot(rsiMA, "RSI-based MA", color=color.yellow)
+rsiUpperBand = hline(70, "RSI Upper Band", color=#787B86)
+midline = hline(50, "RSI Middle Band", color=color.new(#787B86, 50))
+rsiLowerBand = hline(30, "RSI Lower Band", color=#787B86)
+fill(rsiUpperBand, rsiLowerBand, color=color.rgb(126, 87, 194, 90), title="RSI Background Fill")
+bbUpperBand = plot(isBB ? rsiMA + ta.stdev(rsi, maLengthInput) * bbMultInput : na, title = "Upper Bollinger Band", color=color.green)
+bbLowerBand = plot(isBB ? rsiMA - ta.stdev(rsi, maLengthInput) * bbMultInput : na, title = "Lower Bollinger Band", color=color.green)
+fill(bbUpperBand, bbLowerBand, color= isBB ? color.new(color.green, 90) : na, title="Bollinger Bands Background Fill")
+
+midLinePlot = plot(50, color = na, editable = false, display = display.none)
+fill(rsiPlot, midLinePlot, 100, 70, top_color = color.new(color.green, 0), bottom_color = color.new(color.green, 100),  title = "Overbought Gradient Fill")
+fill(rsiPlot, midLinePlot, 30,  0,  top_color = color.new(color.red, 100), bottom_color = color.new(color.red, 0),      title = "Oversold Gradient Fill")
+
+// Divergence
+lookbackRight = 5
+lookbackLeft = 5
+rangeUpper = 60
+rangeLower = 5
 bearColor = color.red
 bullColor = color.green
-hiddenBullColor = color.new(color.green, 80)
-hiddenBearColor = color.new(color.red, 80)
 textColor = color.white
 noneColor = color.new(color.white, 100)
-osc = ta.rsi(src, len)
 
-plot(osc, title="RSI", linewidth=2, color=#2962FF)
-hline(50, title="Middle Line", color=#787B86, linestyle=hline.style_dotted)
-obLevel = hline(70, title="Overbought", color=#787B86, linestyle=hline.style_dotted)
-osLevel = hline(30, title="Oversold", color=#787B86, linestyle=hline.style_dotted)
-fill(obLevel, osLevel, title="Background", color=color.rgb(33, 150, 243, 90))
-
-plFound = na(ta.pivotlow(osc, lbL, lbR)) ? false : true
-phFound = na(ta.pivothigh(osc, lbL, lbR)) ? false : true
+plFound = na(ta.pivotlow(rsi, lookbackLeft, lookbackRight)) ? false : true
+phFound = na(ta.pivothigh(rsi, lookbackLeft, lookbackRight)) ? false : true
 _inRange(cond) =>
 	bars = ta.barssince(cond == true)
 	rangeLower <= bars and bars <= rangeUpper
 
 //------------------------------------------------------------------------------
 // Regular Bullish
-// Osc: Higher Low
+// rsi: Higher Low
 
-oscHL = osc[lbR] > ta.valuewhen(plFound, osc[lbR], 1) and _inRange(plFound[1])
+rsiHL = rsi[lookbackRight] > ta.valuewhen(plFound, rsi[lookbackRight], 1) and _inRange(plFound[1])
 
 // Price: Lower Low
 
-priceLL = low[lbR] < ta.valuewhen(plFound, low[lbR], 1)
-bullCondAlert = priceLL and oscHL and plFound
-bullCond = plotBull and bullCondAlert
+priceLL = low[lookbackRight] < ta.valuewhen(plFound, low[lookbackRight], 1)
+bullCondAlert = priceLL and rsiHL and plFound
+bullCond = showDivergence and bullCondAlert
 
 plot(
-     plFound ? osc[lbR] : na,
-     offset=-lbR,
+     plFound ? rsi[lookbackRight] : na,
+     offset=-lookbackRight,
      title="Regular Bullish",
      linewidth=2,
      color=(bullCond ? bullColor : noneColor),
@@ -52,8 +75,8 @@ plot(
      )
 
 plotshape(
-	 bullCond ? osc[lbR] : na,
-	 offset=-lbR,
+	 bullCond ? rsi[lookbackRight] : na,
+	 offset=-lookbackRight,
 	 title="Regular Bullish Label",
 	 text=" Bull ",
 	 style=shape.labelup,
@@ -63,53 +86,21 @@ plotshape(
 	 )
 
 //------------------------------------------------------------------------------
-// Hidden Bullish
-// Osc: Lower Low
-
-oscLL = osc[lbR] < ta.valuewhen(plFound, osc[lbR], 1) and _inRange(plFound[1])
-
-// Price: Higher Low
-
-priceHL = low[lbR] > ta.valuewhen(plFound, low[lbR], 1)
-hiddenBullCondAlert = priceHL and oscLL and plFound
-hiddenBullCond = plotHiddenBull and hiddenBullCondAlert
-
-plot(
-	 plFound ? osc[lbR] : na,
-	 offset=-lbR,
-	 title="Hidden Bullish",
-	 linewidth=2,
-	 color=(hiddenBullCond ? hiddenBullColor : noneColor),
-	 display = display.pane
-	 )
-
-plotshape(
-	 hiddenBullCond ? osc[lbR] : na,
-	 offset=-lbR,
-	 title="Hidden Bullish Label",
-	 text=" H Bull ",
-	 style=shape.labelup,
-	 location=location.absolute,
-	 color=bullColor,
-	 textcolor=textColor
-	 )
-
-//------------------------------------------------------------------------------
 // Regular Bearish
-// Osc: Lower High
+// rsi: Lower High
 
-oscLH = osc[lbR] < ta.valuewhen(phFound, osc[lbR], 1) and _inRange(phFound[1])
+rsiLH = rsi[lookbackRight] < ta.valuewhen(phFound, rsi[lookbackRight], 1) and _inRange(phFound[1])
 
 // Price: Higher High
 
-priceHH = high[lbR] > ta.valuewhen(phFound, high[lbR], 1)
+priceHH = high[lookbackRight] > ta.valuewhen(phFound, high[lookbackRight], 1)
 
-bearCondAlert = priceHH and oscLH and phFound
-bearCond = plotBear and bearCondAlert
+bearCondAlert = priceHH and rsiLH and phFound
+bearCond = showDivergence and bearCondAlert
 
 plot(
-	 phFound ? osc[lbR] : na,
-	 offset=-lbR,
+	 phFound ? rsi[lookbackRight] : na,
+	 offset=-lookbackRight,
 	 title="Regular Bearish",
 	 linewidth=2,
 	 color=(bearCond ? bearColor : noneColor),
@@ -117,8 +108,8 @@ plot(
 	 )
 
 plotshape(
-	 bearCond ? osc[lbR] : na,
-	 offset=-lbR,
+	 bearCond ? rsi[lookbackRight] : na,
+	 offset=-lookbackRight,
 	 title="Regular Bearish Label",
 	 text=" Bear ",
 	 style=shape.labeldown,
@@ -127,40 +118,5 @@ plotshape(
 	 textcolor=textColor
 	 )
 
-//------------------------------------------------------------------------------
-// Hidden Bearish
-// Osc: Higher High
-
-oscHH = osc[lbR] > ta.valuewhen(phFound, osc[lbR], 1) and _inRange(phFound[1])
-
-// Price: Lower High
-
-priceLH = high[lbR] < ta.valuewhen(phFound, high[lbR], 1)
-
-hiddenBearCondAlert = priceLH and oscHH and phFound
-hiddenBearCond = plotHiddenBear and hiddenBearCondAlert
-
-plot(
-	 phFound ? osc[lbR] : na,
-	 offset=-lbR,
-	 title="Hidden Bearish",
-	 linewidth=2,
-	 color=(hiddenBearCond ? hiddenBearColor : noneColor),
-	 display = display.pane
-	 )
-
-plotshape(
-	 hiddenBearCond ? osc[lbR] : na,
-	 offset=-lbR,
-	 title="Hidden Bearish Label",
-	 text=" H Bear ",
-	 style=shape.labeldown,
-	 location=location.absolute,
-	 color=bearColor,
-	 textcolor=textColor
-	 )
-
-alertcondition(bullCondAlert, title='Regular Bullish Divergence', message="Found a new Regular Bullish Divergence, `Pivot Lookback Right` number of bars to the left of the current bar")
-alertcondition(hiddenBullCondAlert, title='Hidden Bullish Divergence', message='Found a new Hidden Bullish Divergence, `Pivot Lookback Right` number of bars to the left of the current bar')
-alertcondition(bearCondAlert, title='Regular Bearish Divergence', message='Found a new Regular Bearish Divergence, `Pivot Lookback Right` number of bars to the left of the current bar')
-alertcondition(hiddenBearCondAlert, title='Hidden Bearish Divergence', message='Found a new Hidden Bearish Divergence, `Pivot Lookback Right` number of bars to the left of the current bar')
+alertcondition(bullCondAlert, title='Regular Bullish Divergence', message="Found a new Regular Bullish Divergence, `Pivot Lookback Right` number of bars to the left of the current bar.")
+alertcondition(bearCondAlert, title='Regular Bearish Divergence', message='Found a new Regular Bearish Divergence, `Pivot Lookback Right` number of bars to the left of the current bar.')
